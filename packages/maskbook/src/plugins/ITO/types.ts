9155689c217247078cca16bbabb73554bb8ff8ea
guanbinrui <guanbinrui@dimension.im>
2020-12-01 14:09:29 +0800

    chore: add ito plugin

--- packages/maskbook/src/plugins/ITO/types.ts
+++ packages/maskbook/src/plugins/ITO/types.ts
@@ -1,56 +1,22 @@
+import type { EthereumNetwork, EthereumTokenType } from '../../web3/types'
 import type { ERC20TokenRecord } from '../Wallet/database/types'
-import type { EthereumTokenType, EthereumNetwork } from '../../web3/types'
-export interface ITORecord {
-    id: string
-    from: string
-    payload: ITOJSONPayload
-}
 
-export interface ITORecordDatabase extends ITORecord {
-    type: 'ito'
-}
-
-export enum ITOStatus {
-    clained = 'claimed',
-    expired = 'expired',
-    empty = 'empty',
-    refunded = 'refunded',
-}
-
-export interface ITOMetaData {
-    data: string
-    sender?: string
-}
-
-export interface ITOAvailability {
-    token_address: string
-    balance: string
-    tottal: String
-    claimed: string
-    expired: boolean
-    ifclaimed: boolean
-}
-
-export interface ITOJSONPayload {
+export interface ITO_JSONPayload {
     contract_version: number
     contract_address: string
     rpid: string
-
     password: string
+    shares: number
     sender: {
         address: string
         name: string
         message: string
     }
+    is_random: boolean
     total: string
-    create_time: number
-    duraction: number
+    creation_time: number
+    duration: number
     network?: EthereumNetwork
-    from_token_type: EthereumTokenType.Ether | EthereumTokenType.ERC20
-    from_token?: Pick<ERC20TokenRecord, 'address' | 'name' | 'decimals' | 'symbol'>
-    to_token_type: EthereumTokenType.Ether | EthereumTokenType.ERC20
-    to_token?: Pick<ERC20TokenRecord, 'address' | 'name' | 'decimals' | 'symbol'>
-    num: number
-    enddate: string
-    ratio: number
+    token_type: EthereumTokenType.Ether | EthereumTokenType.ERC20
+    token?: Pick<ERC20TokenRecord, 'address' | 'name' | 'decimals' | 'symbol'>
 }
--- packages/maskbook/src/plugins/PluginUI.ts
+++ packages/maskbook/src/plugins/PluginUI.ts
@@ -14,11 +14,7 @@ import { FileServicePluginDefine } from './FileService/define'
 import { TraderPluginDefine } from './Trader/define'
 import { Election2020PluginDefine } from './Election2020/define'
 import { TransakPluginDefine } from './Transak/define'
-import { COTM_PluginDefine } from './COTM/define'
-
-import { ITOPluginDefine } from './ITO/define'
-
-import { ITOPluginDefine } from './ITO/define'
+import { ITO_PluginDefine } from './ITO/define'
 
 plugins.add(WalletPluginDefine)
 plugins.add(GitcoinPluginDefine)
@@ -28,6 +24,6 @@ if (Flags.poll_enabled) plugins.add(PollsPluginDefine)
 if (Flags.trader_enabled) plugins.add(TraderPluginDefine)
 if (Flags.transak_enabled) plugins.add(TransakPluginDefine)
 if (Flags.election2020_enabled) plugins.add(Election2020PluginDefine)
-if (Flags.COTM_enabled) plugins.add(COTM_PluginDefine)
+if (Flags.ito_enabled) plugins.add(ITO_PluginDefine)
 if (process.env.STORYBOOK) plugins.add(StorybookPluginDefine)
 plugins.add(ITOPluginDefine)
